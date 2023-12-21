59fc576996fc0e66492bb5e119737ad53b687690
Lahess <80914959+LahessAimere@users.noreply.github.com>
2023-12-21 09:31:09 +0100

    Exercice refait

--- src/Array.cpp
+++ src/Array.cpp
@@ -1,46 +1,85 @@
 #include "Array.hpp"
+#include <iostream>
 
 namespace Data {
+    template<class Datatype>
     
-    inline Array::Array (int size)
+    inline Array<Datatype>::Array (int size)
     {
+        _array = new Datatype[size];
+        _size = size;
 
+        for (int i = 0; i < size; ++i) 
+        {
+            _array[i] = Datatype();
+        }
     }
 
-    inline Array::~Array ()
+    template<class Datatype>
+    inline Array<Datatype>::~Array ()
     {
-    
+        delete[] _array;
     }
 
-    // void Array::resize (int size)
-    // {
+    template<class Datatype>
+    void Array<Datatype>::resize (int size)
+    {
+        Datatype* newArray = new Datatype[size];
+        
+        int min;
+        if (size < _size) 
+        {
+            min = size;
+        } 
+        else 
+        {
+            min = _size;
+        }
 
-    // }
+        for (int i = 0; i < min; ++i) 
+        {
+            newArray[i] = _array[i];
+        }
+        delete[] _array;
+        _array = newArray;
+    }
 
-    // void Array::insert(int item, int index)
-    // {
+    template<class Datatype>
+    void Array<Datatype>::insert(Datatype item, int index)
+    {
+        for (int i = _size - 1; i > index; --i) {
+        _array[i] = _array[i - 1];
+    }
+        _array[index] = item;
+    }
 
-    // }
+    template<class Datatype>
+    void Array<Datatype>::remove(int index)
+    {
+        for (int i = index + 1; i < _size; ++i) 
+        {
+            _array[i - 1] = _array[i];
+        }
+        _size--;
+    }
 
-    // void Array::remove(int index)
-    // {
+    template<class Datatype>
+    int Array<Datatype>::size()
+    {
+        return _size;
+    }
 
-    // }
+    template<class Datatype>
+    Datatype& Array<Datatype>::operator[](int index)
+    {
+        return _array[index];
+    }
 
-    // int Array::size()
-    // {
+    template<class Datatype>
+    Array<Datatype>::operator Datatype* ()
+    {
+        return _array;
+    }
 
-    // }
-
-    // int& Array::operator[](int index)
-    // {
-
-    // }
-
-
-    // Array::operator Datatype* ()
-    // {
-
-    // }
-
+    template class Array<int>; 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,41 @@
 
 using namespace Data;
 
+template<class Datatype>
+void displayArray(Array<Datatype>& array)
+{
+    std::cout << "[";
+    for (int i = 0; i < array._size; i++)
+    {
+        std::cout << array._array[i] << " ";
+    }
+    std::cout << "]" << std::endl;
+}
+
 int main()
 {
-    std::cout << "COUCOU" << std::endl;
+    Array<int> intArray(5);
+    displayArray(intArray);
+
+    intArray[0] = 75;
+    intArray[1] = 91;
+    intArray[2] = 5;
+    intArray[3] = 0;
+    intArray[4] = 46;
+
+    displayArray(intArray);
+
+    intArray.resize(10);
+
+    displayArray(intArray);
+
+    intArray.insert(67, 3);
+
+    displayArray(intArray);
+
+    intArray.remove(0);
+
+    displayArray(intArray);
 
     return 0;
 }
